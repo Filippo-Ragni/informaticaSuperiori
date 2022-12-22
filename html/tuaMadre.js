7b43769ae263f9491b256dc86f2c899443c7da82
Filippo-Ragni <73989287+Filippo-Ragni@users.noreply.github.com>
2022-12-22 12:15:41 +0100

    Update tuaMadre.js

--- html/tuaMadre.js
+++ html/tuaMadre.js
@@ -1,10 +1,23 @@
 let coda = [];
 
-const addLast = () => {
-    coda.push(document.getElementById("insertName").value);
+const printLista = () => {
     let string = "";
     for (e of coda){
         string += `<li> ${e} </li><INPUT type = "button" value = "rimuovi" onclick = "delElement();"/>`;
     }
     document.getElementById("list").innerHTML = string;
+}
+
+const addLast = () => {
+    coda.push(document.getElementById("insertName").value);
+	printLista();
+}
+
+const delFirst = () => {
+	document.getElementById("delName").value = coda.shift();
+	printLista();
+}
+
+const delElement = () => {
+	
 }
