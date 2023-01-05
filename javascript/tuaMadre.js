d68d1e96d7cf3f5652e1dd2439174974e4a0722e
Filippo-Ragni <filippo.ragni.2005@gmail.com>
2023-01-05 15:34:44 +0100

    corrected tuamadre.js

--- javascript/tuaMadre.js
+++ javascript/tuaMadre.js
@@ -3,12 +3,10 @@ let coda = [];
 const printLista = () => {
     let string = "";
 
-// problema con la funzione printLista: l'argomento passato alla funzione delElement sarà sempre l'indice dell'ultimo elemento della lista
-// ho pensato a mettere un id al tag <li>, ma come argomento a delElement dovrei passare comunque il getElementById col Number("${i}") come argomento, il ché non risolverebbe niente
     coda.forEach ((e, i) =>{
         string += `<li> 
-        ${e} 
-        <INPUT type = "button" value = "rimuovi" onclick = "delElement(${i});"/>
+        ${e}
+        <INPUT type = "button" value = "rimuovi" onclick = "delElement(${i});">
         </li>`;
     })
     document.getElementById("list").innerHTML = string;
@@ -25,6 +23,6 @@ const delFirst = () => {
 }
 
 const delElement = (index) => {
-	coda.pop(index);
-	printLista();
+    delete coda[index];
+    printLista();
 }
