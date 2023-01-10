861aae8aa27cdba95a11e2d8864f5e2bde872618
Filippo-Ragni <filippo.ragni.2005@gmail.com>
2023-01-10 19:43:57 +0100

    slider gatti

--- javascript/fileJs102.js
+++ javascript/fileJs102.js
@@ -1,32 +1,21 @@
-let immages = [imm1.jpg, imm2.jpg, imm3.jpg];
-let currentImg = document.getElementById("immID");
+let currentImg = 0;
 
 const ImmPrev = () => {
-    let prevImm = 0;
-    for (index in immages){
-        if (currentImg.src == immages[i]){
-            prevImm = i-=1;
-        }
-    }
-    if (prevImm == -1){
-        currentImg.src = immages[2];
+    if (currentImg == 0){
+        currentImg = 2;
     }
     else{
-        currentImg.src = immages[prevImm];
+        currentImg -= 1;
     }
+    document.getElementById("immID").src = "./imgGatti/imm" + currentImg + ".jpg";
 }
 
-const ImmNext = () => {    
-    let nextImm = 0;
-    for (index in immages){
-        if (currentImg.src == immages[i]){
-            nextImm = i+=1;
-        }
-    }
-    if (nextImm == 3){
-        currentImg.src = immages[0];
+const ImmNext = () => {
+    if (currentImg == 2){
+        currentImg = 0;
     }
     else{
-        currentImg.src = immages[nextImm];
+        currentImg += 1;
     }
+    document.getElementById("immID").src = "./imgGatti/imm" + currentImg + ".jpg";
 }
